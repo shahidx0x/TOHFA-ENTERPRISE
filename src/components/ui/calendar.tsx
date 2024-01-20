2d61ab2026a7cba883ab65a44a08672cd28bb388
Mufizul Islam <121678815+mufizul27@users.noreply.github.com>
2024-01-20 23:29:42 +0600

    second  commit

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,7 +54,7 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-5 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
