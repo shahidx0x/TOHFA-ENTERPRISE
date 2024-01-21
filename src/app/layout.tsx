e39229f3f302fd8e00dae524cd24e5a0938f65ef
shahidx0x <dev.imshahid@gmail.com>
2024-01-21 21:22:31 +0600

    Remove unused code and update layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,7 @@ import "./globals.css";
 
 import { Navbar } from "@/components/shared/Navbar";
 import StoreProvider from "./providers/StoreProvider";
-import { AuthProvider } from "./providers/AuthProvider";
 import { ThemeProvider } from "./providers/ThemeProvider";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,14 +18,12 @@ export default function RootLayout({ children }: any) {
     <>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
-          <AuthProvider>
-            <StoreProvider>
-              <ThemeProvider>
-                <Navbar />
-                {children}
-              </ThemeProvider>
-            </StoreProvider>
-          </AuthProvider>
+          <StoreProvider>
+            <ThemeProvider>
+              <Navbar />
+              {children}
+            </ThemeProvider>
+          </StoreProvider>
         </body>
       </html>
     </>
