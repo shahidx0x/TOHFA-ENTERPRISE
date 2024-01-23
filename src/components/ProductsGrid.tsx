53f48ad4e251ee28364452e75a237e9646abe69b
shahidx0x <dev.imshahid@gmail.com>
2024-01-23 23:18:05 +0600

    Refactor code to improve performance and readability

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React from "react";
 import bannerImageOne from "../../assets/products/Ameera Ghee Toast.png";
+import { Card } from "./ui/card";
 
 const Productgrid = () => {
   return (
@@ -56,213 +57,24 @@ const Productgrid = () => {
         </div>
 
         <div className=" grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-y-12 lg:gap-x-8 sm:gap-y-10 sm:gap-x-6 gap-y-6 lg:mt-12 mt-10">
-          <div className=" relative ">
-            <div className=" absolute top-0 left-0 py-2 px-4 bg-white bg-opacity-50 ">
-              <p className="text-xs leading-3 text-gray-800">New</p>
-            </div>
-            <div className=" relative group">
-              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-              <Image src={bannerImageOne} width={600} height={50} alt="" />
-              <div className=" absolute bottom-0 p-8 w-full opacity-0 group-hover:opacity-100">
-                <button className=" font-medium text-base leading-4 text-gray-800 bg-white py-3 w-full">
-                  Add to bag
-                </button>
-                <button className=" bg-transparent font-medium text-base leading-4 border-2 border-white py-3 w-full mt-2 text-white">
-                  Quick View
-                </button>
+          <div className="max-w-xs rounded-md shadow-md bg-gray-50 text-gray-800">
+            <Image
+              src={bannerImageOne}
+              alt=""
+              className="object-cover object-center w-full rounded-t-md h-56 bg-gray-500"
+            />
+            <div className="flex flex-col justify-between p-6 space-y-8">
+              <div className="space-y-2">
+                <p className="bg-gray-600 font-bold text-white rounded-full w-28 flex justify-center">
+                  Toast
+                </p>
+                <h2 className="text-2xl font-semibold tracki">GHEE TOAST</h2>
+                <p className="text-gray-800 font-mono font-bold">2000 PCS</p>
               </div>
+              <button className=" hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-gray-800 py-4 rounded-md md:px-16 md:w-auto w-full lg:mt-28 md:mt-12 mt-10 text-white font-medium text-base leading-4">
+                Add to Cart
+              </button>
             </div>
-            <p className=" font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
-              GHEE TOAST
-            </p>
-            <p className=" font-semibold text-xl leading-5 text-gray-800 mt-4">
-              2000PCS
-            </p>
-            <p className=" font-normal text-base leading-4 text-gray-600 mt-4">
-              10 categories
-            </p>
-          </div>
-          <div className=" relative ">
-            <div className=" absolute top-0 left-0 py-2 px-4 bg-white bg-opacity-50 ">
-              <p className="text-xs leading-3 text-gray-800">New</p>
-            </div>
-            <div className=" relative group">
-              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-              <Image src={bannerImageOne} width={600} height={50} alt="" />
-              <div className=" absolute bottom-0 p-8 w-full opacity-0 group-hover:opacity-100">
-                <button className=" font-medium text-base leading-4 text-gray-800 bg-white py-3 w-full">
-                  Add to bag
-                </button>
-                <button className=" bg-transparent font-medium text-base leading-4 border-2 border-white py-3 w-full mt-2 text-white">
-                  Quick View
-                </button>
-              </div>
-            </div>
-            <p className=" font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
-              GHEE TOAST
-            </p>
-            <p className=" font-semibold text-xl leading-5 text-gray-800 mt-4">
-              2000PCS
-            </p>
-            <p className=" font-normal text-base leading-4 text-gray-600 mt-4">
-              10 categories
-            </p>
-          </div>
-          <div className=" relative ">
-            <div className=" absolute top-0 left-0 py-2 px-4 bg-white bg-opacity-50 ">
-              <p className="text-xs leading-3 text-gray-800">New</p>
-            </div>
-            <div className=" relative group">
-              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-              <Image src={bannerImageOne} width={600} height={50} alt="" />
-              <div className=" absolute bottom-0 p-8 w-full opacity-0 group-hover:opacity-100">
-                <button className=" font-medium text-base leading-4 text-gray-800 bg-white py-3 w-full">
-                  Add to bag
-                </button>
-                <button className=" bg-transparent font-medium text-base leading-4 border-2 border-white py-3 w-full mt-2 text-white">
-                  Quick View
-                </button>
-              </div>
-            </div>
-            <p className=" font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
-              GHEE TOAST
-            </p>
-            <p className=" font-semibold text-xl leading-5 text-gray-800 mt-4">
-              2000PCS
-            </p>
-            <p className=" font-normal text-base leading-4 text-gray-600 mt-4">
-              10 categories
-            </p>
-          </div>
-          <div className=" relative ">
-            <div className=" absolute top-0 left-0 py-2 px-4 bg-white bg-opacity-50 ">
-              <p className="text-xs leading-3 text-gray-800">New</p>
-            </div>
-            <div className=" relative group">
-              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-              <Image src={bannerImageOne} width={600} height={50} alt="" />
-              <div className=" absolute bottom-0 p-8 w-full opacity-0 group-hover:opacity-100">
-                <button className=" font-medium text-base leading-4 text-gray-800 bg-white py-3 w-full">
-                  Add to bag
-                </button>
-                <button className=" bg-transparent font-medium text-base leading-4 border-2 border-white py-3 w-full mt-2 text-white">
-                  Quick View
-                </button>
-              </div>
-            </div>
-            <p className=" font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
-              GHEE TOAST
-            </p>
-            <p className=" font-semibold text-xl leading-5 text-gray-800 mt-4">
-              2000PCS
-            </p>
-            <p className=" font-normal text-base leading-4 text-gray-600 mt-4">
-              10 categories
-            </p>
-          </div>
-          <div className=" relative ">
-            <div className=" absolute top-0 left-0 py-2 px-4 bg-white bg-opacity-50 ">
-              <p className="text-xs leading-3 text-gray-800">New</p>
-            </div>
-            <div className=" relative group">
-              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-              <Image src={bannerImageOne} width={600} height={50} alt="" />
-              <div className=" absolute bottom-0 p-8 w-full opacity-0 group-hover:opacity-100">
-                <button className=" font-medium text-base leading-4 text-gray-800 bg-white py-3 w-full">
-                  Add to bag
-                </button>
-                <button className=" bg-transparent font-medium text-base leading-4 border-2 border-white py-3 w-full mt-2 text-white">
-                  Quick View
-                </button>
-              </div>
-            </div>
-            <p className=" font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
-              GHEE TOAST
-            </p>
-            <p className=" font-semibold text-xl leading-5 text-gray-800 mt-4">
-              2000PCS
-            </p>
-            <p className=" font-normal text-base leading-4 text-gray-600 mt-4">
-              10 categories
-            </p>
-          </div>
-          <div className=" relative ">
-            <div className=" absolute top-0 left-0 py-2 px-4 bg-white bg-opacity-50 ">
-              <p className="text-xs leading-3 text-gray-800">New</p>
-            </div>
-            <div className=" relative group">
-              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-              <Image src={bannerImageOne} width={600} height={50} alt="" />
-              <div className=" absolute bottom-0 p-8 w-full opacity-0 group-hover:opacity-100">
-                <button className=" font-medium text-base leading-4 text-gray-800 bg-white py-3 w-full">
-                  Add to bag
-                </button>
-                <button className=" bg-transparent font-medium text-base leading-4 border-2 border-white py-3 w-full mt-2 text-white">
-                  Quick View
-                </button>
-              </div>
-            </div>
-            <p className=" font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
-              GHEE TOAST
-            </p>
-            <p className=" font-semibold text-xl leading-5 text-gray-800 mt-4">
-              2000PCS
-            </p>
-            <p className=" font-normal text-base leading-4 text-gray-600 mt-4">
-              10 categories
-            </p>
-          </div>
-          <div className=" relative ">
-            <div className=" absolute top-0 left-0 py-2 px-4 bg-white bg-opacity-50 ">
-              <p className="text-xs leading-3 text-gray-800">New</p>
-            </div>
-            <div className=" relative group">
-              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-              <Image src={bannerImageOne} width={600} height={50} alt="" />
-              <div className=" absolute bottom-0 p-8 w-full opacity-0 group-hover:opacity-100">
-                <button className=" font-medium text-base leading-4 text-gray-800 bg-white py-3 w-full">
-                  Add to bag
-                </button>
-                <button className=" bg-transparent font-medium text-base leading-4 border-2 border-white py-3 w-full mt-2 text-white">
-                  Quick View
-                </button>
-              </div>
-            </div>
-            <p className=" font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
-              GHEE TOAST
-            </p>
-            <p className=" font-semibold text-xl leading-5 text-gray-800 mt-4">
-              2000PCS
-            </p>
-            <p className=" font-normal text-base leading-4 text-gray-600 mt-4">
-              10 categories
-            </p>
-          </div>
-          <div className=" relative ">
-            <div className=" absolute top-0 left-0 py-2 px-4 bg-white bg-opacity-50 ">
-              <p className="text-xs leading-3 text-gray-800">New</p>
-            </div>
-            <div className=" relative group">
-              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full"></div>
-              <Image src={bannerImageOne} width={600} height={50} alt="" />
-              <div className=" absolute bottom-0 p-8 w-full opacity-0 group-hover:opacity-100">
-                <button className=" font-medium text-base leading-4 text-gray-800 bg-white py-3 w-full">
-                  Add to bag
-                </button>
-                <button className=" bg-transparent font-medium text-base leading-4 border-2 border-white py-3 w-full mt-2 text-white">
-                  Quick View
-                </button>
-              </div>
-            </div>
-            <p className=" font-normal text-xl leading-5 text-gray-800 md:mt-6 mt-4">
-              GHEE TOAST
-            </p>
-            <p className=" font-semibold text-xl leading-5 text-gray-800 mt-4">
-              2000PCS
-            </p>
-            <p className=" font-normal text-base leading-4 text-gray-600 mt-4">
-              10 categories
-            </p>
           </div>
         </div>
 
